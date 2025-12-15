f18f0256e3e787c174957cd7569eb31134a21d3c
Umintyu-Okinawa <rionofficial1008@gmail.com>
2025-12-15 16:10:48 +0900

    型 & Context（50m, 仮データで動作確認）

--- react-study/src/pages/page1.js
+++ react-study/src/pages/page1.js
@@ -1,9 +1,43 @@
+import React from "react";
+
+import { BookMarksProvider, useBookmark } from "../context/BookMarksContext";
+
+const PageContent = () => {
+  const { bookmarks } = useBookmark();
+
+  return (
+    <div>
+      <h1>page1</h1>
+      <ul>
+        {bookmarks.map((book) => (
+          <li key={book.id}>{book.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
 const Page1 = () => {
   return (
-    <>
-      <h1>page1</h1>
-    </>
+    <BookMarksProvider>
+      <PageContent />
+    </BookMarksProvider>
   );
 };
 
 export default Page1;
+
+// import { useState } from "react";
+// import { useBookmark } from "../context/BookMarksContext";
+// import { useContext, createContext } from "react";
+// import { Booktype } from "../context/BookMarksContext";
+
+// const Page1 = () => {
+//   return (
+//     <>
+//       <h1>page1</h1>
+//     </>
+//   );
+// };
+
+// export default Page1;
