05fbd91e88b2d4bd92aaf9a8564d6abe5cee8a99
Umintyu-Okinawa <rionofficial1008@gmail.com>
2025-12-18 11:51:29 +0900

    14.商品サービスの作成

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ItemsModule } from '../items/items.module';
 import { ItemsController } from './items/items.controller';
+import { ItemsService } from './items/items.service';
 
 @Module({
   imports: [ItemsModule],
   controllers: [AppController, ItemsController],
-  providers: [AppService],
+  providers: [AppService, ItemsService],
 })
 export class AppModule {}
 
