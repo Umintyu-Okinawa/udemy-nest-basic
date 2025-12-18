e57a652cc4cefe3f1f3cee795f9c0febcc3b3e88
Umintyu-Okinawa <rionofficial1008@gmail.com>
2025-12-18 11:16:07 +0900

    12.商品コントローラーの作成

---
+++ src/app.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ItemsModule } from '../items/items.module';
+import { ItemsController } from './items/items.controller';
+
+@Module({
+  imports: [ItemsModule],
+  controllers: [AppController, ItemsController],
+  providers: [AppService],
+})
+export class AppModule {}
+
+
