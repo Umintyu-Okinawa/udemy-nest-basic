0a684b43e2d695de4e5248953a2f3853bb8e410a
Umintyu-Okinawa <rionofficial1008@gmail.com>
2025-12-18 15:45:35 +0900

    26.Class Validatorをりようしたバリエーション

--- src/items/dto/create-item.dto.ts
+++ src/items/dto/create-item.dto.ts
@@ -1,5 +1,20 @@
+import { IsString, IsNotEmpty, MaxLength, IsInt, Min ,IsOptional} from "class-validator";
+
 export class CreateItemDto {
+@IsString()
+@IsNotEmpty()
+@MaxLength(40)
   name!: string;
+
+
+@IsInt()
+@Min(1)
   price!: number;
+
+
+
+  @IsOptional()
+  @IsString()
+  @MaxLength(100)
   description?: string;
 }
