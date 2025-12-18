87dd2dd0f97ad85b189bda52177289f7928b8f1e
Umintyu-Okinawa <rionofficial1008@gmail.com>
2025-12-18 14:01:10 +0900

    17.CRUD機能の実装(Create)

--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -1,10 +1,16 @@
 import { Injectable } from '@nestjs/common';
-
+import { Item } from './items.model';
 
 @Injectable()
 export class ItemsService {
+  private readonly items: Item[] = [];
 
-    findAll() {
-        return 'This is ItemsService';
-    }
+  findAll() {
+    return this.items;
+  }
+
+  create(item: Item) {
+    this.items.push(item);
+    return item;
+  }
 }
