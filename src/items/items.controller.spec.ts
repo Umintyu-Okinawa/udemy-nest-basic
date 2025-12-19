d0a1bcfdceba47dea85def68a5f67795e712ea35
Umintyu-Okinawa <rionofficial1008@gmail.com>
2025-12-19 15:23:51 +0900

    56.Jestによるユニットテスト

--- src/items/items.controller.spec.ts
+++ src/items/items.controller.spec.ts
@@ -1,18 +1,41 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ItemsController } from './items.controller';
+import { ItemsService } from './items.service';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 describe('ItemsController', () => {
   let controller: ItemsController;
+  let service: ItemsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ItemsController],
+      providers: [
+        ItemsService,
+        {
+          provide: PrismaService,
+          useValue: {
+            item: {
+              findMany: jest.fn(),
+              findUnique: jest.fn(),
+              create: jest.fn(),
+              update: jest.fn(),
+              delete: jest.fn(),
+            },
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<ItemsController>(ItemsController);
+    service = module.get<ItemsService>(ItemsService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should be defined service', () => {
+    expect(service).toBeDefined();
   });
 });
