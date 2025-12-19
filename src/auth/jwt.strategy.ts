bd002e00614295e9afc0ddde7bf2bea6e57dc489
Umintyu-Okinawa <rionofficial1008@gmail.com>
2025-12-19 14:34:59 +0900

    51.JWT認証の実装Part1

---
+++ src/auth/jwt.strategy.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { JwtPayload } from 'src/types/jwtpayload';
+import { RequestUser } from 'src/types/requestUser';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: process.env.JWT_SECRET || 'default-secret',
+    });
+  }
+
+  async validate(payload: JwtPayload): Promise<RequestUser> {
+    return {
+      id: payload.sub,
+      name: payload.name,
+      status: payload.status,
+    };
+  }
+}
